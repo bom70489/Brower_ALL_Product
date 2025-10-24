3f649b17171d0a2085dc213a35405bd926d1e3f4
basrockerghost <bassparty75@gamil.com>
2025-10-24 15:34:28 +0700

    fix

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -3,7 +3,7 @@ import Navber from "./components/Navber"
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Login from "./components/Login";
-import Sidebar from "./components/sidebar";
+import Sidebar from "./components/Sidebar";
 import { Route, Routes } from "react-router-dom";
 import Add from "./pages/Add";
 import List from "./pages/List";
